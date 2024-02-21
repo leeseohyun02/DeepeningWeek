0c96ecad3b483b7083f84b48ac109c1a73d7cb12
leeseohyun02 <78461967+leeseohyun02@users.noreply.github.com>
2024-02-21 16:39:16 +0900

    feat : 오브젝트 생성

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -21,7 +21,42 @@ public class GameManager : MonoBehaviour
 
     private void NextCircle()
     {
-        GetCircle();
+        Circle newCircle = GetCircle();
+        lastCircle = newCircle;
+
+        StartCoroutine(WaitNext());
     }
 
+    //코루틴
+    IEnumerator WaitNext()
+    {
+        while (lastCircle != null) // while에서 yield를 쓰지 않으면 무한루프
+        {
+            yield return null;
+        }
+        // 오브젝트가 없으면 2.5초 뒤에 생성되도록
+        yield return new WaitForSeconds(2.5f);
+
+        NextCircle();
+    }
+
+    public void TouchDown()
+    {
+        if(lastCircle == null) // null이면 반환
+        {
+            return;
+        }
+
+        lastCircle.Drag();
+    }
+
+    public void TouchUp()
+    {
+        if(lastCircle == null)
+        {
+            return;
+        }
+        lastCircle.Drop();
+        lastCircle = null; // 떨어뜨리면 null
+    }
 }
