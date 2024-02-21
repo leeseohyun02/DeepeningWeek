54f28217681c41aad367906ffc3c928c694db26f
leeseohyun02 <78461967+leeseohyun02@users.noreply.github.com>
2024-02-21 15:03:28 +0900

    feat : 오브젝트 위치 고정

--- My project/Assets/Scripts/Circle.cs
+++ My project/Assets/Scripts/Circle.cs
@@ -13,6 +13,20 @@ public class Circle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float leftBorder = -5f + transform.localScale.x / 2f;
+        float rightBorder = 5f - transform.localScale.x / 2f;
+
+        if(mousePos.x < leftBorder)
+        {
+            mousePos.x = leftBorder;
+        }
+        else if(mousePos.x > rightBorder)
+        {
+            mousePos.x = rightBorder;
+        }
+        mousePos.y = 8;
+        mousePos.z = 0;
+        transform.position = Vector3.Lerp(transform.position,mousePos,0.2f);
     }
 }
