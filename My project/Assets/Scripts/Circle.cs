e96ec42caeca527f04970dc4c87d1da308566be4
leeseohyun02 <78461967+leeseohyun02@users.noreply.github.com>
2024-02-21 15:16:36 +0900

    feat : 드래그 이벤트 로직

--- My project/Assets/Scripts/Circle.cs
+++ My project/Assets/Scripts/Circle.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public class Circle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public bool isDrag;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float leftBorder = -5f + transform.localScale.x / 2f;
-        float rightBorder = 5f - transform.localScale.x / 2f;
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float leftBorder = -5f + transform.localScale.x / 2f;
+            float rightBorder = 5f - transform.localScale.x / 2f;
 
-        if(mousePos.x < leftBorder)
-        {
-            mousePos.x = leftBorder;
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+            mousePos.y = 8;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
         }
-        else if(mousePos.x > rightBorder)
-        {
-            mousePos.x = rightBorder;
-        }
-        mousePos.y = 8;
-        mousePos.z = 0;
-        transform.position = Vector3.Lerp(transform.position,mousePos,0.2f);
+        
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        _rigidbody.simulated = true; // 인스펙터에서 false 상태인 simulated를 true로
     }
 }
